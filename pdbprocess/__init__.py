5db5a5f20b3688ebb14e89b2146cd2c630960451
leijinmin <jinmin.lei@aalto.fi>
2015-05-20 15:11:03 +0300

    1st try

---
+++ pdbprocess/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'lejinmin'
+print 'pdb file processing'
+
+fname = '1A3W'
+f = open(fname,'rb')
+
+f.close()
---
+++ pdbprocess/getSSEs.py
@@ -0,0 +1,5 @@
+__author__ = 'lejinmin'
+
+def getSSEs():
+    print 'hello'
+    return 1
